40e2b6a6ea914f9cff99a12b2af1bff363cb11e9
modmuss50 <modmuss50@gmail.com>
2017-07-05 16:14:41 +0100

    Add concrete and test dependencies

---
+++ src/test/java/techreborn/test/TechRebornTest.java
@@ -0,0 +1,14 @@
+package techreborn.test;
+
+import org.junit.Test;
+
+/**
+ * Created by modmuss50 on 05/07/2017.
+ */
+public class TechRebornTest {
+
+	@Test
+	public void test(){
+		//TODO test stuff
+	}
+}
